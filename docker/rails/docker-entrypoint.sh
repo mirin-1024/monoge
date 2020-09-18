fe0c7c9ae9b45a9523734ab477a9051f0d467b8a
mirin-1024 <lastexperiment1024@gmail.com>
2020-09-19 03:17:57 +0900

    Create basic docker environment

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -2,25 +2,22 @@
 
 set -e
 
-
 if [ ! -e Gemfile ]; then
-
   cp -a $READY_RAILS_DIR/Gemfile .
   touch Gemfile.lock
 
   bundle install
-  rails new . -d mysql -f
+  rails new . -d mysql -f --skip-test
 
   cp -a $READY_RAILS_DIR/database.yml \
     $READY_RAILS_DIR/puma.rb \
     $READY_RAILS_DIR/application.rb config/
-
   mkdir -p tmp/sockets
 
   rm -r $READY_RAILS_DIR
 
   until rails db:drop &> /dev/null; do
-    >&2 echo “MySQL is unavailable - sleeping”
+    >&2 echo "MySQL is unavailable - sleeping"
     sleep 1
   done
 
@@ -28,7 +25,8 @@ if [ ! -e Gemfile ]; then
 
 elif [ -e $READY_RAILS_DIR ]; then
   bundle install
+  yarn install --check-files
   rm -r $READY_RAILS_DIR
 fi
 
-exec “$@“
+exec "$@"
