26a744de5da3ec789622b6ff53e58a71712fecdf
mirin-1024 <lastexperiment1024@gmail.com>
2020-09-26 18:53:09 +0900

    modify CircleCI setting

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "StaticPages", type: :request do
 
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /contact" do
     it "returns http success" do
-      get "/static_pages/contact"
+      get "/contact"
       expect(response).to have_http_status(:success)
     end
   end
