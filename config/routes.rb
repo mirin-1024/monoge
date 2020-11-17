b94d2114fc63c3e89d67b50f40885e475fa4ae17
mirin-1024 <lastexperiment1024@gmail.com>
2020-11-18 00:51:41 +0900

    Implement basic login system #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
+  get '/contact',   to: 'static_pages#contact'
+  get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
--- spec/system/signup_users_spec.rb
+++ spec/system/signup_users_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "SignupUsers", type: :system do
-  describe "Sign up with valid information" do
+  context "正確な情報でログイン" do
     before do
-    visit signup_path
+      visit signup_path
       fill_in "名前", with: "Foobar"
       fill_in "メールアドレス", with: "foobar@example.com"
       fill_in "パスワード", with: "password"
@@ -11,12 +11,12 @@ RSpec.describe "SignupUsers", type: :system do
       click_button "登録"
     end
     subject { page }
-    it "has success message" do
+    example "成功時のフラッシュメッセージを表示" do
       is_expected.to have_selector(".alert-success")
     end
   end
 
-  describe "Sign up with invalid information" do
+  context "不正確な情報でログイン" do
     before do
       visit signup_path
       fill_in "名前", with: ""
@@ -26,7 +26,7 @@ RSpec.describe "SignupUsers", type: :system do
       click_button "登録"
     end
     subject { page }
-    it "has error message" do
+    example "エラーメッセージを表示" do
       is_expected.to have_selector(".alert-danger")
     end
   end
