32df9fa06ada4d9ad8930e2beb11f03b5bf72a0b
mirin-1024 <lastexperiment1024@gmail.com>
2020-12-12 01:49:11 +0900

    Implement follow function #13

---
+++ spec/models/relationship_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Relationship, type: :model do
+  let(:user) { create(:user) }
+  let(:other_user) { create(:other_user) }
+  let!(:relationship) { create(:relationship, follower_id: user.id, followed_id: other_user.id) }
+
+  example "ユーザーをフォローできる" do
+    expect(relationship).to be_valid
+  end
+
+  example "フォロワーのIDが存在する" do
+    relationship.follower_id = nil
+    expect(relationship).to_not be_valid
+  end
+
+  example "フォローしているユーザーのIDが存在する" do
+    relationship.followed_id = nil
+    expect(relationship).to_not be_valid
+  end
+end
