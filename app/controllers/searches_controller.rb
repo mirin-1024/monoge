bfc4bfaa5d01bd7226daac3305ded6e997db27d4
mirin-1024 <lastexperiment1024@gmail.com>
2020-12-22 02:07:34 +0900

    Modify the view of search results #15

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,17 @@
 class SearchesController < ApplicationController
   def search
-    @model = params[:model]
-    @keyword = params[:content]
-    @search_results = search_for(@model, @keyword)
-    redirect_to search_path
+    @search_model = params[:model]
+    @search_word = params[:keyword]
+    @search_results = search_for(@search_model, @search_word)
+    render 'searches/search'
   end
 
 private
 
   def search_for(model, keyword)
-    if model == User
+    if model == "User"
       User.where("name LIKE ?", "%#{keyword}%").paginate(page: params[:page])
-    else model == Micropost
+    else model == "Micropost"
       Micropost.where("content LIKE ?", "%#{keyword}%").paginate(page: params[:page])
     end
   end
