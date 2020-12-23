cb8b6cd0971192d671cbb27f4cd14dec218977a7
mirin-1024 <lastexperiment1024@gmail.com>
2020-12-23 23:47:20 +0900

    Modify basic comment function #23

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
+  before_action :logged_in_user, only: [:create]
+  before_action :correct_user, only: [:destroy]
 
   def create
     @comment = current_user.comments.build(comment_params)
@@ -15,12 +16,20 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    flash[:success] = "コメントを削除しました"
+    redirect_to @comment.micropost
   end
 
   private
 
     def comment_params
       params.require(:comment).permit(:content)
+    end
+
+    def correct_user
+      @comment = current_user.comments.find_by(id: params[:id])
+      redirect_to root_url if @comment.nil?
     end
 end
