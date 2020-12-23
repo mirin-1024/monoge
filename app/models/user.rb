1e9257cb8569a2af9e14e7be3323a81e8e40eff5
mirin-1024 <lastexperiment1024@gmail.com>
2020-12-23 17:37:40 +0900

    Add validation to Comment model then add has_many relations to User and Micropost model #23

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships,
                        source: :follower
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
