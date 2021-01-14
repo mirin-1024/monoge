bfabe8d0f33483fe24d913f6c460832533e26979
mirin-1024 <lastexperiment1024@gmail.com>
2021-01-14 23:59:50 +0900

    Permit container IP #33

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # dockerコンテナのipを許可
+  config.web_console.whitelisted_ips = '192.168.160.1'
 end
