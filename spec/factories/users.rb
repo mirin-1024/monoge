182f34ac069f17bcc37402e9c6b66a46140ad19b
mirin-1024 <lastexperiment1024@gmail.com>
2021-01-17 16:14:24 +0900

    Create guest login test #55

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,48 +1,52 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, traits: %i[non_admin activated] do
     name { 'Foo Bar' }
     email { 'foobar@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
-    activated { true }
-    activated_at { Time.zone.now }
   end
 
-  factory :invalid_user, class: 'User' do
+  factory :invalid_user, class: 'User', traits: %i[non_admin activated] do
     name { 'Invalid User' }
     email { "#{'a' * 39}@example.com" }
     password { 'password' }
     password_confirmation { 'passward' }
-    activated { true }
-    activated_at { Time.zone.now }
   end
 
-  factory :other_user, class: 'User' do
+  factory :other_user, class: 'User', traits: %i[non_admin activated] do
     name { 'Foobar1' }
     email { 'foobar1@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
-    activated { true }
-    activated_at { Time.zone.now }
   end
 
-  factory :test_user, class: 'User' do
+  factory :test_user, class: 'User', traits: %i[non_admin activated] do
     name { Faker::Name.name }
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
-    activated { true }
-    activated_at { Time.zone.now }
   end
 
-  factory :admin_user, class: 'User' do
+  factory :admin_user, class: 'User', traits: %i[admin activated] do
     name { 'Admin User' }
     email { 'adminuser@example.com' }
     password { 'password' }
     password_confirmation { 'password' }
+  end
+
+  factory :guest_user, class: 'User', traits: %i[non_admin activated] do
+    name { 'Guest User' }
+    email { 'guest@example.com' }
+    password { 'password' }
+    password_confirmation { 'password' }
+  end
+
+  trait :admin do
     admin { true }
-    activated { true }
-    activated_at { Time.zone.now }
+  end
+
+  trait :non_admin do
+    admin { false }
   end
 
   trait :activated do
