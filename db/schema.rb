a7a2503fa46f27b786605293feedab9844e6e663
mirin-1024 <lastexperiment1024@gmail.com>
2021-01-22 11:39:07 +0900

    Finish migrating #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_054648) do
+ActiveRecord::Schema.define(version: 2021_01_22_023551) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 2021_01_13_054648) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.string "image"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
