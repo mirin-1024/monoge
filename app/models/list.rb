0dd157f33d53760bf66df0618fb60bed13efe76a
mirin-1024 <lastexperiment1024@gmail.com>
2021-01-27 21:50:02 +0900

    Add routing and relationship with users #78

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+  belongs_to :user
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   resources :relationships, only: %i[create destroy]
   resources :likes, only: %i[create destroy]
   resources :comments, only: %i[create destroy]
+  resources :list, only: %i[create destroy show]
 end
